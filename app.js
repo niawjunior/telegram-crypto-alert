3817b0dfcdb1afd5f5980e38d6d91b614d2e83c6
niawkung <niawkung@gmail.com>
2018-07-20 17:11:05 +0700

    fix

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const token = config.token;
 const axios = require('axios')
 const bot = new TelegramBot(token, {polling: true})
 const img_url = 'https://github.com/niawjunior/telegram-crypto-alert/blob/master/logo.png?raw=true'
+const firebase = require('firebase')
+const firebase_config = config.firebase_config;
+firebase.initializeApp(firebase_config);
 
 async function main() {
     return await  axios.get(`https://api.coinmarketcap.com/v2/ticker/?convert=THB&limit=10`)
+}
+async function bitcoin() {
+    return await axios.get('https://api.coinmarketcap.com/v2/ticker/1/?convert=THB')
 }
 
 bot.onText(/\/start/, (msg) => {
@@ -16,13 +22,14 @@ bot.onText(/\/start/, (msg) => {
                 "keyboard": [["เช็คราคา", "ตั้งค่าการแจ้งเตือน"]]
                 }
         }
-        bot.sendMessage(msg.chat.id,"\n⚔️⚔️ คุณสมบัติของบอทที่สามารถทำได้ ⚔️⚔️\n\n 1. ตรวจสอบราคาของเหรียญสกุลต่างๆ(20 อันดับแรก) \n 2. ตั้งค่าการการแจ้งเตือน \n\n",option)
+        user_id = msg.from.id
+        bot.sendMessage(msg.chat.id,"\n⚔️⚔️ คุณสมบัติของบอทที่สามารถทำได้ ⚔️⚔️\n\n 1. ตรวจสอบราคาของเหรียญสกุลต่างๆ(20 อันดับแรก) \n 2. การแจ้งเตือน ทุกๆ 10 นาที (ขณะนี้รองรับเฉพาะสกุลเหรียญ Bitcoin) \n\n",option)
     })
 })
 
 bot.on('message', (msg) => {
-    const send_text = msg.text
-    if (send_text.toString().indexOf('เช็คราคา') === 0) {
+    const check_price = msg.text
+    if (check_price.toString().indexOf('เช็คราคา') === 0) {
         bot.sendMessage(msg.chat.id,"พิมพ์ชื่อสกุลเหรียญที่ต้องการตรวจสอบ\n")
     }
 })
@@ -44,4 +51,66 @@ bot.on('message',(msg) => {
         }
     })
 })
+bot.on('message', (msg) => {
+    const set_alert = msg.text
+    if(set_alert.toString().indexOf('ตั้งค่าการแจ้งเตือน') === 0) {
+        var option = {
+            "reply_markup": {
+                "keyboard": [["สมัครรับการแจ้งเตือน", "ยกเลิกรับการแจ้งเตือน"]]
+                }
+        }
+        bot.sendMessage(msg.chat.id,"\n สมัคร หรือ ยกเลิกการแจ้งเตือน \n",option)
+    }
+})
+
+bot.on('message', (msg) => {
+    const set_time = msg.text
+    if(set_time.toString().indexOf('สมัครรับการแจ้งเตือน') === 0) {
+        firebase.database().ref('Users').child(msg.from.id).update({
+            updated_At: Date.now(),
+            telegram_id: msg.from.id,
+            status: true
+        }).then(() => {
+            bot.sendMessage(msg.chat.id,"\n สมัครรับการแจ้งเตือนแล้ว \n")
+            get_profile(msg.from.id)
+        })
+    }
+})
+
+bot.on('message', (msg) => {
+    const set_calcel = msg.text
+    if(set_calcel.toString().indexOf('ยกเลิกรับการแจ้งเตือน') === 0) {
+        firebase.database().ref('Users').child(msg.from.id).update({
+            updated_At: Date.now(),
+            telegram_id: msg.from.id,
+            status: false
+        }).then(() => {
+            bot.sendMessage(msg.chat.id,"\n ยกเลิกรับการแจ้งเตือนแล้ว \n")
+            get_profile(msg.from.id)
+        })
+    }
+})
+
+
+
+get_profile=(user_id)=> {
+    setInterval(() => {
+    firebase.database().ref('Users').child(user_id).once('value', snap => {
+        var telegram_id = snap.key
+        if(snap.val().status === true) {
+                bitcoin().then(data => {
+                    let price_thb = data.data.data.quotes.THB.price
+                    let price_usd = data.data.data.quotes.USD.price
+                    bot.sendMessage(telegram_id,`❤️❤️ BTC (Bitcoin) ❤️❤️ \n\n THB = ${price_thb.toLocaleString()} \n USD = ${price_usd}`)
+                })
+            }
+        })
+    },600000)
+}
+
+
+
+     
+
+
 
--- config.js
+++ config.js
@@ -1,4 +1,13 @@
 // telegram token
 const token = '666089928:AAHfc6RBjVunpHusY3ClcLXcfo1I0hMOneM'
+const config = {
+    apiKey: "AIzaSyC9Jin_Gk7Y2CqKD83pn4kZbSwMBTowubg",
+    authDomain: "crypto-alert-52895.firebaseapp.com",
+    databaseURL: "https://crypto-alert-52895.firebaseio.com",
+    projectId: "crypto-alert-52895",
+    storageBucket: "crypto-alert-52895.appspot.com",
+    messagingSenderId: "775850960902"
+};
 
+exports.firebase_config = config;
 exports.token = token;
